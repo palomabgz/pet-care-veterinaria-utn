7acc116adadeb0042df81605c86dab5bbcf13102
Paloma González <palomabgz@gmail.com>
2024-11-17 17:55:16 -0300

    mysql

--- app.js
+++ app.js
@@ -5,21 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var pool = require('./models/bd');
 
 var indexRouter = require('./routes/index'); //routes/index.js
-// var usersRouter = require('./routes/users'); //routes/users.js
-
-// // manejador de ruta 1:
-// var contactoRouter = require('./routes/contacto'); //routes/contacto.js
-
-// // manejador de ruta 2:
-// var pruebaRouter = require('./routes/prueba'); //routes/prueba.js
-
-// // manejador de ruta 3:
-// var novedadesRouter = require('./routes/novedades'); //routes/novedades.js
-
-// // manejador de ruta 4:
-// var destacadosRouter = require('./routes/destacados'); //routes/destacados.js
 
 var app = express();
 
@@ -34,34 +22,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
-// // rutas:
-// app.use('/contacto', contactoRouter);
-// app.use('/prueba', pruebaRouter);
-// app.use('/novedades', novedadesRouter);
-// app.use('/destacados', destacadosRouter);
-
-// // ruta 1:
-// app.get('/contacto', function(req, res) {
-//   res.send('pantalla de contacto')
-// })
-
-// // ruta 2:
-// app.get('/prueba', function(req, res) {
-//   res.send('pantalla de prueba')
-// })
-
-// // ruta 3:
-// app.get('/novedades', function(req, res) {
-//   res.send('pantalla de novedades')
-// })
-
-// // ruta 4:
-// app.get('/destacados', function(req, res) {
-//   res.send('pantalla de destacados')
-// })
-
+// consulta:
+pool.query('select * from comentarios_bd').then(function (resultados){
+  console.log(resultados)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
